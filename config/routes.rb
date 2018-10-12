49bc7f055bacf2d59cfb8f10b7aa0436da4fd8c2
ma3a <ma3a@xaker.ru>
2018-10-12 11:05:01 +0500

    add namespace creator

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
   resources :users
-  resources :posts
+  resources :posts, only: [:index, :show]
+
+  namespace :creator do
+    resources :posts, except: [:index, :show]
+  end 
 end
